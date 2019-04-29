61335bffcd8579107787c853c720e2313e14100c
Naved Islam <navedcoded@gmail.com>
2019-04-29 17:32:38 +0500

    better app strcture

--- src/App.js
+++ src/App.js
@@ -1,16 +1,88 @@
 import React, { Component } from 'react';
-import './App.css';
-import Layl from './Layl.js'
+import jsonp from 'jsonp'
+import { format, addDays, parse, differenceInMilliseconds, addMilliseconds } from 'date-fns';
+import logo from "./logo.svg"
 
-class App extends Component {
+function Table (props) {
+  return (
+    <div>
+      <img src={logo} alt="Logo"/>
+      <p>You are in: <strong>{props.city}, {props.country}</strong></p>
+      <p>Half the night is at: <strong>{props.first_third}</strong></p>
+      <p>The last third of the night starts at: <strong>{props.last_third}</strong></p>
+    </div>
+  )
+}
+
+class Layl extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      city: "",
+      country: "",
+      times: "Loading…",
+      today: new Date(), //TODO: question: set date like this, or set by API return data?
+      tomorrow: addDays(new Date(), 1),
+      fajr: null,
+      maghrib: null,
+    }
+    this.getTimes = this.getTimes.bind(this)
+  }
+  getTimes() {
+    jsonp(
+      `https://muslimsalat.com/weekly.json?key=1f3f533bb4b16343e373be5de3601247s`,
+      null, //TODO: use muwaqqit API instead?
+      (err, data) => {
+        if (err) {
+          console.error(err.message)
+        } else {
+          console.log(`getTimes location: `+data.city)
+          console.log(data.items[1].fajr)
+          let today = data.items[0].date_for
+        let tomorrow = data.items[1].date_for
+        let maghrib = parse(`${today} ${data.items[0].maghrib}`)
+        let fajr = parse(`${tomorrow} ${data.items[1].fajr}`)
+        console.log(maghrib)
+        console.log(fajr)
+        
+        let interval = differenceInMilliseconds(fajr, maghrib) / 6
+        console.log(interval)
+        let times = []
+        for (let i = 0; i < 7; i++) {
+          times.push(addMilliseconds(maghrib, i*interval))
+        }
+        this.setState({
+          maghrib,
+          fajr,
+          today,
+          tomorrow,
+          first_third: format(times[3], "hh:mm aa"),
+          last_third: format(times[4], "hh:mm aa"),
+          city: data.city,
+          country: data.country,
+        })
+        console.log(Object.values(times).map((time) => format(time, 'hh mm aa')))
+        }
+      }
+    )
+  }
+  componentDidMount() {
+    this.getTimes()
+    }
+  componentDidUpdate() {
+  }
   render() {
     return (
-      <div className="App">
-        
-        <Layl />
+      <div>
+        <Table 
+        first_third={this.state.first_third}
+        last_third={this.state.last_third}
+        city={this.state.city}
+        country={this.state.country}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default Layl
