709ee3586cb120300527be77e3a7c984fb6674a4
Naved Islam <navedcoded@gmail.com>
2019-04-29 20:33:21 +0500

    fix city console log

--- src/App.js
+++ src/App.js
@@ -32,10 +32,10 @@ class Layl extends Component {
   getTimes() {
     fetchJsonp(`https://extreme-ip-lookup.com/json/`)
       .then(response => {
-        console.log('location: '+response)
         return response.json()
       }).then(json => {
         let city = json.city
+        console.log('location: '+city)
         fetchJsonp(`https://muslimsalat.com/${city}/weekly.json?key=1f3f533bb4b16343e373be5de3601247s`)
           .then(response => {
             console.log(response)
