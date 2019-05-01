d1166f1d345c5771c5047d7259c2ab7118f550a9
Naved Islam <navedcoded@gmail.com>
2019-05-01 08:49:49 +0500

    add all second screen content

--- src/App.js
+++ src/App.js
@@ -18,6 +18,69 @@ function Table (props) {
   )
 }
 
+function Info (props) {
+  return (
+    <div >
+      <div style={{position: 'absolute',bottom:'0%', left:'auto'}}>
+        <p><a>How does this work? 👇🏼</a></p>
+      </div>
+      <div style={{position: 'absolute',top:'100%', display:'flex', flexDirection:'column'}}>
+        <div style={{display:'flex', flexDirection:'row'}}>
+          <div style={{display:'flex', flexDirection:'column', textAlign:'left'}}>
+            <h1>Why calculate the night?</h1>
+            <p>The night is really significant for the believer, host to great times for worship and 
+              prayer:</p>
+            <blockquote>“Be ever steadfast in ⸢observing⸣ the Prayer at the declining of the sun, 
+              until the darkening of the night. Moreover, ⸢hold fast to⸣ the Quran's recitation 
+              at the dawn ⸢Prayer⸣. Indeed, the recitation at dawn is ever witnessed ⸢by hosts of 
+              angels and believers⸣” (The Gracious Qur’an, 17:78-79).</blockquote>
+            <blockquote>The Messenger of God - blessings and peace be upon him, his family, 
+              and companions - said, “Hold fast to night prayer, for it was the way of 
+              the righteous before you, a way of drawing closer to your Lord, an expiation 
+              for wrong deeds, and a shield from sin” (Tirmidhi).</blockquote>
+            <p>Although any prayer performed after the Isha prayer qualifies as the 
+              virtuous night prayer, the most virtuous time is in it’s later parts.</p>
+            <p>Additionally, knowing these divisions of the night allows us to follow the 
+              sleep and worship lifestyles of the prophets, may peace be upon them all.</p>
+            <p>More than just virtue, it is important to know divisions of the night so we 
+              may fulfill obligations like the Isha prayer within the right time.</p>
+            <p><a href="https://www.gettoby.com/p/jfjfjlg8mpw2">Sources and further reading.</a></p>
+          </div>
+          <div style={{display:'flex', flexDirection:'column'}}>
+            <h1>Details of calculations</h1>
+            <p>The night is defined to begin at the time of Maghrib prayer, right after sunset, 
+              and lasts till the start of Fajr time, which is dawn. As mentioned previously, 
+              dividing the night into sixths provides the most flexibility and so we will 
+              follow that method.</p>
+            <p>Maghrib marks the beginning of the first-sixth of the night and Fajr is the 
+              end of the last-sixth of the night. Dividing the length of night by six gives 
+              the length of one-sixth of the night. That value can be added to Maghrib time 
+              to find the start of the second-sixth of the night. Then, adding the length of 
+              one-sixth of the night to the start of the second-sixth of night gives the beginning 
+              of the third-sixth of the night, and so on.</p>
+            <p>This website determines your location automatically based on your IP address, 
+              and finds the appropriate times for your location. Note, this may be slightly 
+              inaccurate depending on internet configuration or completely wrong if using a VPN. 
+              Other options for determining location will be added later, by the Grace of God.</p>
+            <p>Also, although the calculation of Maghrib time is quite clear and anywhere in 
+              the world, the calculation of Fajr relies upon the chosen angle of calculation. 
+              This best angle for calculation varies from place to place, and even depends on 
+              the seasons in a given location. This app tries to automatically choose the best 
+              calculation method for your location, but it may not be the most accurate.</p>
+            <p>All these details and warnings aren’t pretty, but I think they’re part of 
+              producing apps for such important purposes. May God accept our worship.</p>
+          </div>
+          
+      </div>
+      <div style={{textAlign:'center', position:'relative', top:'200%'}}>
+        <p>Made with 💗 by @mrislam_</p>
+      </div>
+        </div>
+    </div>
+    
+  )
+}
+
 
 class Layl extends Component {
   constructor(props) {
@@ -47,7 +110,7 @@ class Layl extends Component {
   }
   callApi(city) {
     fetchJsonp(`https://muslimsalat.com/${city}/weekly.json?key=1f3f533bb4b16343e373be5de3601247s`)
-      .then(response => {
+      .then(response => { //TODO: update timeout becuase it can be slow
         console.log(response)
         return response.json()
       }).then(json => {
@@ -98,6 +161,7 @@ class Layl extends Component {
         country={this.state.country}
         maghrib={this.state.maghrib}
         />
+        <Info />
       </div>
     )
   }
