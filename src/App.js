3cedc67f1c79d0fe9a2a59d671590a0a9e267fa6
Naved Islam <navedcoded@gmail.com>
2019-05-24 14:20:51 +0500

    better warning

--- src/App.js
+++ src/App.js
@@ -71,9 +71,10 @@ function Info () { //TODO: move texts to a different file
               calculation of Fajr relies upon different methods. The best method 
               is different in each place, and can change with the seasons too. 
               This app uses a well-tested calculation method with the proven <em>"Adhan"</em> prayer
-              calculation software—but it may be incorrect. 
-              Thus, <strong>please safeguard your worship by keeping enough 
-              time before or after the times given by this app</strong>.</p>
+              calculation software—but it may be incorrect.</p>
+            <p> Thus, please <strong>double-check the Maghrib and Fajr times</strong> given by this app – 
+              if they are correct then the other times will be correct. Also, safeguard your 
+              worship by keeping enough time before or after the times given by any app.</p>
             <p>These warnings aren't pretty, but they're part of 
               responsible app development. May Allah accept our worship, <em>ameen</em>.</p>
             <p><a href="https://www.gettoby.com/p/jfjfjlg8mpw2">Sources and further reading.</a></p>
